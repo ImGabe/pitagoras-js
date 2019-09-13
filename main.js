fb61bb14429f29b7a96f51ec23a23e2264826a96
imgabe <gabrielpmonte@hotmail.com>
2019-09-13 15:45:11 -0300

    now accepts floats

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ let catb = document.getElementById("cat-b");
 let hipc = document.getElementById("hip-c");
 let out = document.getElementById("result");
 
-let solver = (a = NaN, b = NaN, c = NaN) => {
+let solver = (a, b, c) => {
     return Math.sqrt(c - b) || Math.sqrt(c - a) || Math.sqrt(a + b) || "Não consigo calcular...";
 }
 
 function result() {
-    let a = parseInt(cata.value) ** 2;
-    let b = parseInt(catb.value) ** 2;
-    let c = parseInt(hipc.value) ** 2;
+    let a = Number(cata.value) ** 2 || NaN;
+    let b = Number(catb.value) ** 2 || NaN;
+    let c = Number(hipc.value) ** 2 || NaN;
     out.innerHTML = (a + b == c) ? `${a} + ${b} = ${c}` : (!a || !b || !c) ? solver(a, b, c) : `A conta está errada...`;
 }
 
